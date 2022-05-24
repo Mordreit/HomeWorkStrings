88ab4f10a7b09b10cac5bab0fb4f0696fcb37595
Mordreit <spirit_x93@mail.ru>
2022-05-24 22:56:43 +0400

    Готовое дз!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задание 1
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = "";
+        fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
+
+        // Задание 2
+
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для административного отчета - " + fullName);
+
+        // Задание 3
+
+        fullName = "Ivanov Ivan Ivanovich";
+        fullName = fullName.replace(' ', ';');
+        System.out.println("Данные ФИО сотрудника для административного отдела - " + fullName);
+
+        // Задание 4
+
+        fullName = "Иванов Семен Семёнович";
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
